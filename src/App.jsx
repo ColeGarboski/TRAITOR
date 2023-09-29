5f14749bdda3b69845dc90e3e26d4171ad9ad273
Cole Garboski <cole.garboski@snhu.edu>
2023-09-29 12:42:25 -0400

    Add in loading state to remove Submit button to not waste tokens

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import { useState } from 'react';
-import axios from 'axios'; // Make sure to install axios: npm install axios
+import axios from 'axios'; 
 import './App.css';
 
 function App() {
-    const [text, setText] = useState(''); // To hold the user input
-    const [response, setResponse] = useState(''); // To hold the server response
+    const [text, setText] = useState(''); 
+    const [response, setResponse] = useState(''); 
+    const [loading, setLoading] = useState(false);
 
     const handleSubmit = async () => {
+        setLoading(true); // Set loading to true when request starts
         try {
             const result = await axios.post('http://127.0.0.1:5000/generate', { prompt: text });
             setResponse(result.data.response);
         } catch (error) {
             console.error('There was an error sending the request', error);
+            setResponse('There was an error sending the request');
+        } finally {
+            setLoading(false); // Set loading back to false once request completes
         }
     };
 
@@ -19,14 +24,14 @@ function App() {
         <div className="App">
             <h1>trAItor</h1>
             <div className="inputContainer">
-            <textarea
-                value={text}
-                onChange={(e) => setText(e.target.value)}
-                placeholder="Enter your text here"
-                rows={5}
-                cols={50}
-            />
-                <button onClick={handleSubmit}>Submit</button>
+                <textarea
+                    value={text}
+                    onChange={(e) => setText(e.target.value)}
+                    placeholder="Enter your text here"
+                    rows={5}
+                    cols={50}
+                />
+                {!loading && <button onClick={handleSubmit}>Submit</button>}
             </div>
             {response && <div><h2>Response:</h2><p>{response}</p></div>}
         </div>
