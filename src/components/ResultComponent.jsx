01b138eb3305870d4f84c32e5063ef2b8bd62d61
dude <137600619+dude@users.noreply.github.com>
2023-12-11 13:16:19 -0500

    Results page styling :100:

--- src/components/ResultComponent.jsx
+++ src/components/ResultComponent.jsx
@@ -1,48 +1,25 @@
 import React from 'react';
 
-function Result({ status, testName, evaluation }) {
-  const isPass = status === 'pass';
-
-  return (
-    <div className={`p-4 max-h-max shadow-md rounded-md mx-4 my-4 ${isPass ? 'bg-green-50' : 'bg-red-50'}`}>
-      <style>
-        {`
-          .result-pre {
-            white-space: pre-wrap;
-            word-wrap: break-word;
-            overflow-wrap: break-word;
-          }
-        `}
-      </style>
-      <div className={`flex items-center ${isPass ? 'text-green-500' : 'text-red-500'}`}>
-        {/* SVG icons */}
-        {isPass ? (
-          <svg
-            className="w-6 h-6 mr-2"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            aria-hidden="true"
-          >
-            {/* SVG path */}
-          </svg>
-        ) : (
-          <svg
-            className="w-6 h-6 mr-2"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            aria-hidden="true"
-          >
-            {/* SVG path */}
-          </svg>
-        )}
-        {isPass ? 'Pass' : 'Fail'}
-      </div>
-      <div className="mt-2 font-bold text-slate-700">{testName}</div>
-      <div className="mt-1 text-slate-600 text-sm result-pre">
-        {evaluation}
-      </div>
-    </div>
-  );
+function Result({description, evaluation}) {
+    return (
+        <div className="flex justify-center items-center"> {/* Flex container to center the Result component */}
+            <div className="rounded-lg p-4 bg-white md:w-1/2 "> {/* Neutral background color */}
+                <style>
+                    {`
+                        .result-pre {
+                            white-space: pre-wrap;
+                            word-wrap: break-word;
+                            overflow-wrap: break-word;
+                        }
+                    `}
+                </style>
+                <div className="font-bold text-slate-700">{description}</div>
+                <div className="mt-2 text-slate-600 text-sm result-pre">
+                    {evaluation}
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Result;
