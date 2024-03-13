8cba9ec9d45cf0f0736ee6baca07bf58e1b693a0
dude <137600619+dude@users.noreply.github.com>
2024-03-13 16:35:51 -0400

    ClassTeacherPage Add

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,12 @@ import LandingPage from '/src/components/LandingPage';
 import HomePage from '/src/components/HomePage';
 import ResultsPage from '/src/components/ResultsPage';
 import SignupPage from '/src/components/SignupPage';
+<<<<<<< Updated upstream
 import TeacherPage from '/src/components/TeacherPage';
 import StudentPage from '/src/components/StudentPage';
+=======
+import ClassTeacherPage from "./components/ClassTeacherPage.jsx";
+>>>>>>> Stashed changes
 
 //import './App.css';
 
@@ -20,8 +24,12 @@ function App() {
                         <Route path="/home" element={<HomePage />}></Route>
                         <Route path="/results" element={<ResultsPage />}></Route>
                         <Route path="/signup" element={<SignupPage />}></Route>
+<<<<<<< Updated upstream
                         <Route path="/teacher" element={<TeacherPage />}></Route>
                         <Route path="/student" element={<StudentPage />}></Route>
+=======
+                        <Route path="/class" element={<ClassTeacherPage />}></Route>
+>>>>>>> Stashed changes
                     </Routes>
                 </div>
             </Router>
